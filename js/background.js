ad937f5b945f60f5d986c258cf53835c08b58597
Allen Lee <qinshuoli@microsoft.com>
2020-12-21 12:50:59 +0800

    feat: rm url tip

--- js/background.js
+++ js/background.js
@@ -12,41 +12,41 @@
 //         browser.browserAction.disable(sender.tab.id);
 //     });
 
-const VALID_URLS = ['microsoft.com'];
+// const VALID_URLS = ['microsoft.com'];
 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (!tab.url) return;
+// chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+//     if (!tab.url) return;
 
-    if (
-        // changeInfo.title === "New tab" ||
-        // (tab.url && tab.url.indexOf('msn.com') > -1) ||
-        VALID_URLS.reduce((p, c) => p || tab.url.indexOf(c) > -1, false)
-    ) {
-        // chrome.tabs.insertCSS(tabId, {
-        //     code: `body { background: red !important; }`
-        // }, (...args) => {
-        //     alert(JSON.stringify(args))
-        // });
+//     if (
+//         // changeInfo.title === "New tab" ||
+//         // (tab.url && tab.url.indexOf('msn.com') > -1) ||
+//         VALID_URLS.reduce((p, c) => p || tab.url.indexOf(c) > -1, false)
+//     ) {
+//         // chrome.tabs.insertCSS(tabId, {
+//         //     code: `body { background: red !important; }`
+//         // }, (...args) => {
+//         //     alert(JSON.stringify(args))
+//         // });
 
-        chrome.tabs.executeScript(tabId, {
-            code: `
-                if (!document.getElementById("NTP_HELPER_URL")) {
-                    document.body.innerHTML = '<div id="NTP_HELPER_URL" style="font-size: 10px; background: #c1c1c1">${tab.url}</div>'
-                        + document.body.innerHTML;
-                }
-            `
-        })
+//         chrome.tabs.executeScript(tabId, {
+//             code: `
+//                 if (!document.getElementById("NTP_HELPER_URL")) {
+//                     document.body.innerHTML = '<div id="NTP_HELPER_URL" style="font-size: 10px; background: #c1c1c1">${tab.url}</div>'
+//                         + document.body.innerHTML;
+//                 }
+//             `
+//         })
 
-        // chrome.runtime.sendMessage({
-        //     url: tab.url
-        // });
+//         // chrome.runtime.sendMessage({
+//         //     url: tab.url
+//         // });
 
-        // chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        //     chrome.tabs.sendMessage(
-        //         tabs[0].id,
-        //         { url: tab.url }
-        //     );
-        // });
-    }
-});
+//         // chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+//         //     chrome.tabs.sendMessage(
+//         //         tabs[0].id,
+//         //         { url: tab.url }
+//         //     );
+//         // });
+//     }
+// });
 
